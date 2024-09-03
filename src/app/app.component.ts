31aae3f66ab65a63b32b1d2f6872979c9bf423df
JAPPLET <trianajuan970@gmail.com>
2024-09-03 16:20:28 -0500

    :iphone: Add navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SharedModule } from './shared/shared.module';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SharedModule],
+  imports: [RouterOutlet, SharedModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 import {
   MatSnackBarModule,
   MAT_SNACK_BAR_DEFAULT_OPTIONS,
 } from '@angular/material/snack-bar';
 
-const materialComponents = [MatButtonModule, MatTableModule,MatSnackBarModule];
+const materialComponents = [
+  MatButtonModule,
+  MatTableModule,
+  MatSnackBarModule,
+  MatToolbarModule,
+  MatIconModule
+];
 
 @NgModule({
   declarations: [],
@@ -14,6 +22,6 @@ const materialComponents = [MatButtonModule, MatTableModule,MatSnackBarModule];
   exports: [materialComponents],
   providers: [
     { provide: MAT_SNACK_BAR_DEFAULT_OPTIONS, useValue: { duration: 2500 } },
-  ]
+  ],
 })
 export class MaterialModule {}
